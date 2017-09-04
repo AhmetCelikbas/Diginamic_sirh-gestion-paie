8e5736b2d2fda89091568c33df2ffb398b138a3d
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-09-04 17:47:01 +0200

    TP2 terminé

---
+++ src/main/java/dev/paie/config/ServicesConfig.java
@@ -0,0 +1,10 @@
+package dev.paie.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configurable
+@ComponentScan("dev")
+public class ServicesConfig {
+
+}
